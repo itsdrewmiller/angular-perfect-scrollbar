f26c1da6afdc194ff30eb7c7ab9f399138f2b856
Drew Miller <drew@drewmiller.net>
2013-06-19 15:44:43 -0400

    Explaining hack

--- src/angular-perfect-scrollbar.js
+++ src/angular-perfect-scrollbar.js
@@ -7,11 +7,14 @@ angular.module('perfect_scrollbar', []).directive('perfectScrollbar', function($
 		link: function($scope, $elem, $attr) {
 		    $elem.perfectScrollbar({
 				wheelSpeed: $parse($attr.wheelSpeed)() || 50,
-				wheelPropagation: $parse($attr.wheelPropagation)() || false
+				wheelPropagation: $parse($attr.wheelPropagation)() || false,
+				minScrollbarLength: $parse($attr.minScrollbarLength)() || false,
 			});
 
 			if ($attr.refreshOnChange) {
 				$scope.$watchCollection($attr.refreshOnChange, function(newNames, oldNames) {
+					// I'm not crazy about setting timeouts but it sounds like thie is unavoidable per
+					// http://stackoverflow.com/questions/11125078/is-there-a-post-render-callback-for-angular-js-directive
 					setTimeout(function() { $elem.perfectScrollbar('update'); }, 10);
 				});
 			}
