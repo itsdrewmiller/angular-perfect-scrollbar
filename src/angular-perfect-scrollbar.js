c9b3c620447e360a55880312c560af49d07ae7f7
seldary <seldary@gmail.com>
2014-04-03 18:12:44 +0300

    Using $timeout instead of setTimeout
    This is a recommended practice by angular. Also, returning null
    apparently prevents a memory leak.

--- src/angular-perfect-scrollbar.js
+++ src/angular-perfect-scrollbar.js
@@ -1,4 +1,4 @@
-angular.module('perfect_scrollbar', []).directive('perfectScrollbar', ['$parse', function($parse) {
+angular.module('perfect_scrollbar', []).directive('perfectScrollbar', ['$parse', '$timeout', function($parse) {
   return {
     restrict: 'E',
     transclude: true,
@@ -18,7 +18,10 @@ angular.module('perfect_scrollbar', []).directive('perfectScrollbar', ['$parse',
         $scope.$watchCollection($attr.refreshOnChange, function(newNames, oldNames) {
           // I'm not crazy about setting timeouts but it sounds like thie is unavoidable per
           // http://stackoverflow.com/questions/11125078/is-there-a-post-render-callback-for-angular-js-directive
-          setTimeout(function() { $elem.perfectScrollbar('update'); }, 10);
+          $timeout(function() {
+				$elem.perfectScrollbar('update');
+				return null;
+			}, 10);
         });
       }
 	  
