34e0d4cbc75ae7a89e11c5a4023cb6c8ed00c312
alexk111 <alexkaul@googlemail.com>
2014-06-12 17:48:23 +0800

    No Show bug fix
    Sometimes the scroll bar doesn't appear until scrolling begins.
    Additional Update on mouseenter fixes this.

--- src/angular-perfect-scrollbar.js
+++ src/angular-perfect-scrollbar.js
@@ -4,7 +4,7 @@ angular.module('perfect_scrollbar', []).directive('perfectScrollbar', ['$parse',
     'useKeyboard', 'suppressScrollX', 'suppressScrollY', 'scrollXMarginOffset',
     'scrollYMarginOffset', 'includePadding'
   ];
-  
+
   return {
     restrict: 'E',
     transclude: true,
@@ -12,24 +12,31 @@ angular.module('perfect_scrollbar', []).directive('perfectScrollbar', ['$parse',
     replace: true,
     link: function($scope, $elem, $attr) {
       var options = {};
-      
+
       for (var i=0, l=psOptions.length; i<l; i++) {
         var opt = psOptions[i];
         if ($attr[opt] != undefined) {
           options[opt] = $parse($attr[opt])();
         }
       }
-      
+
       $elem.perfectScrollbar(options);
+
+      function doUpdate() {
+        $scope.$evalAsync(function() {
+          $elem.perfectScrollbar('update');
+        });
+      }
 
       if ($attr.refreshOnChange) {
         $scope.$watchCollection($attr.refreshOnChange, function() {
-          $scope.$evalAsync(function() {
-            $elem.perfectScrollbar('update');
-          });
+          doUpdate();
         });
       }
-      
+      $elem.bind('mouseenter', function(){
+        doUpdate();
+      });
+
       $elem.bind('$destroy', function() {
         $elem.perfectScrollbar('destroy');
       });
