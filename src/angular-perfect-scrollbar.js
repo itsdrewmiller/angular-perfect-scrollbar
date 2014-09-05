d6ae17d3f49df8fb523abc5301b77b988f05d736
Gilad Beeri <giladb.dev@gmail.com>
2014-09-05 09:58:01 +0300

    added refreshOnResize attribute which makes the directive set an event listener for window resize, and update the scrollbar when it triggers

--- src/angular-perfect-scrollbar.js
+++ src/angular-perfect-scrollbar.js
@@ -1,37 +1,46 @@
-angular.module('perfect_scrollbar', []).directive('perfectScrollbar', ['$parse', function($parse) {
+angular.module('perfect_scrollbar', []).directive('perfectScrollbar',
+    ['$parse', '$window', function($parse, $window) {
   var psOptions = [
     'wheelSpeed', 'wheelPropagation', 'minScrollbarLength', 'useBothWheelAxes',
     'useKeyboard', 'suppressScrollX', 'suppressScrollY', 'scrollXMarginOffset',
     'scrollYMarginOffset', 'includePadding'
   ];
-  
+
   return {
     restrict: 'E',
     transclude: true,
     template: '<div><div ng-transclude></div></div>',
     replace: true,
     link: function($scope, $elem, $attr) {
+      function updateScrollbar() {
+          $scope.$evalAsync(function() {
+              $elem.perfectScrollbar('update');
+          });
+      }
+
       var options = {};
-      
+      var jqWindow = angular.element($window);
+
       for (var i=0, l=psOptions.length; i<l; i++) {
         var opt = psOptions[i];
         if ($attr[opt] != undefined) {
           options[opt] = $parse($attr[opt])();
         }
       }
-      
+
       $elem.perfectScrollbar(options);
 
       if ($attr.refreshOnChange) {
-        $scope.$watchCollection($attr.refreshOnChange, function() {
-          $scope.$evalAsync(function() {
-            $elem.perfectScrollbar('update');
-          });
-        });
+        $scope.$watchCollection($attr.refreshOnChange, updateScrollbar);
       }
-      
+
+      if ($attr.refreshOnResize) {
+          jqWindow.on('resize', updateScrollbar);
+      }
+
       $elem.bind('$destroy', function() {
-        $elem.perfectScrollbar('destroy');
+          jqWindow.off('resize', updateScrollbar);
+          $elem.perfectScrollbar('destroy');
       });
     }
   };
