e58344c542900445a6e008fa42a5a5f09a45cb7f
Clint Buhs <cbuhs@atomiclearning.com>
2017-04-24 15:03:46 -0500

    v0.0.6 - Minified JS version; layout tweaks for examples

--- gruntfile.js
+++ gruntfile.js
@@ -4,10 +4,27 @@ module.exports = function (grunt) {
   grunt.initConfig({
     pkg: grunt.file.readJSON('package.json'),
     jshint: {
-      allFiles: ['Gruntfile.js', 'src/**/*.js']
+      all: [
+        // includes
+        'Gruntfile.js',
+        'src/**/*.js',
+        // ignores
+        '!src/**/*.min.js']
+    },
+    uglify: {
+      all: {
+        options: {
+          mangle: false
+        },
+        files: {
+          'src/angular-perfect-scrollbar.min.js': ['src/angular-perfect-scrollbar.js']
+        }
+      }
     }
   });
 
   grunt.loadNpmTasks('grunt-contrib-jshint');
-  grunt.registerTask('default', ['jshint']);
+  grunt.loadNpmTasks('grunt-contrib-uglify');
+
+  grunt.registerTask('default', ['jshint', 'uglify']);
 };
